711192378fd8bdeb812c55b167ce0d99cc0e431e
pak3d11 <pak3d1122@gmail.com>
2018-10-02 08:04:27 +0900

    속도계, 디비 작업중 커밋

---
+++ app/src/main/java/com/zzak/kr/bikecluster/DB/DBHelper.java
@@ -0,0 +1,8 @@
+package com.zzak.kr.bikecluster.DB;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    private static
+}
--- app/src/main/java/com/zzak/kr/bikecluster/view/speedchek/view/FmtSpeedCheck.java
+++ app/src/main/java/com/zzak/kr/bikecluster/view/speedchek/view/FmtSpeedCheck.java
@@ -1,22 +1,29 @@
 package com.zzak.kr.bikecluster.view.speedchek.view;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.compa.gsk.base.BaseFragment;
 import com.zzak.kr.bikecluster.R;
-import com.zzak.kr.bikecluster.view.speedchek.presenter.PresenterSpeedCheck;
 
-public class FmtSpeedCheck extends BaseFragment<PresenterSpeedCheck> {
+public class FmtSpeedCheck extends BaseFragment {
 
-    public static final String FRAGMENT_TAG = FmtSpeedCheck.class.getName();
-    public static BaseFragment newInstance(Bundle bundle){
-        BaseFragment fragment = new FmtSpeedCheck();
-        if(bundle != null){
-            fragment.setArguments(bundle);
-        }
-        return fragment;
-    }
-
+    private TextView currentSpeed;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    double mySpeed, maxSpeed;
 
     @Override
     protected boolean useMainFragment() {
@@ -25,16 +32,63 @@ public class FmtSpeedCheck extends BaseFragment<PresenterSpeedCheck> {
 
     @Override
     protected int inflateLayout() {
+
         return R.layout.fmt_speed_check;
     }
 
     @Override
     protected void viewFindById(View view) {
 
+        currentSpeed = view.findViewById(R.id.current_speed);
     }
 
     @Override
     protected void viewSetting() {
 
+        maxSpeed = mySpeed = 0;
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new SpeedoActionListener();
+
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
     }
+
+    private class SpeedoActionListener implements LocationListener {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            if (location != null) {
+
+                mySpeed = location.getSpeed();
+
+                if (mySpeed > maxSpeed) {
+                    maxSpeed = mySpeed;
+                }
+
+                currentSpeed.setText("\nCurrent Speed : " + mySpeed + "km/h, Max Speed: " + maxSpeed + "km/h");
+            }
+        }
+    }
+
 }
