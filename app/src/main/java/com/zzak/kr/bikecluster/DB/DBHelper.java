bcc62575ff85bb13c1882d9645b35299bf204e51
pak3d11 <pak3d1122@gmail.com>
2018-10-04 09:09:24 +0900

    속도계 구현 중간 커밋

--- app/src/main/java/com/zzak/kr/bikecluster/DB/DBHelper.java
+++ app/src/main/java/com/zzak/kr/bikecluster/DB/DBHelper.java
@@ -1,8 +1,8 @@
-package com.zzak.kr.bikecluster.DB;
-
-import android.database.sqlite.SQLiteOpenHelper;
-
-public class DBHelper extends SQLiteOpenHelper{
-
-    private static
-}
+//package com.zzak.kr.bikecluster.DB;
+//
+//import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class DBHelper extends SQLiteOpenHelper{
+//
+//    private static
+//}
--- app/src/main/java/com/zzak/kr/bikecluster/view/speedchek/view/FmtSpeedCheck.java
+++ app/src/main/java/com/zzak/kr/bikecluster/view/speedchek/view/FmtSpeedCheck.java
@@ -51,11 +51,14 @@ public class FmtSpeedCheck extends BaseFragment {
         locationListener = new SpeedoActionListener();
 
 
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        } else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
         }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
 
     }
 
